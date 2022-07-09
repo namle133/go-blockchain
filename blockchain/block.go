6e632250231b7678712e6b123086b754460931d4
Nam Le <leon.lethanhnam@gmail.com>
2022-07-10 00:42:36 +0700

    blockchain

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,104 +1,132 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-	"time"
+// type Block struct {
+// 	Hash      []byte
+// 	TimeStamp []byte
+// 	Data      []byte
+// 	PreHash   []byte
+// 	Nonce     int
+// }
 
-	"golang.org/x/crypto/bcrypt"
-)
+// type BlockChain struct {
+// 	Blocks []*Block
+// }
 
-type Block struct {
-	Hash      []byte
-	TimeStamp []byte
-	Data      []byte
-	PreHash   []byte
-	Nonce     int
-}
+// func (b *Block) DeriveHash() {
+// 	times := time.Now().String()
+// 	info := bytes.Join([][]byte{[]byte(times), b.Data, b.PreHash}, []byte{})
+// 	hash := sha256.Sum256(info)
+// 	b.Hash = hash[:]
+// 	b.TimeStamp = []byte(times)
+// }
+
+// // func (b *Block) GiveHash(h []byte) {
+// // 	b.Hash = h[:]
+// // }
+
+// func (b *Block) CalculateHash() []byte {
+// 	info := bytes.Join([][]byte{b.TimeStamp, b.Data, b.PreHash}, []byte{})
+// 	hash := sha256.Sum256(info)
+// 	h := hash[:]
+// 	return h
+// }
+
+// func CreatBlock(data string, preHash []byte) *Block {
+
+// 	block := &Block{[]byte{}, []byte{}, []byte(data), preHash, 0}
+// 	// h := Hash1(bytes.Join([][]byte{block.TimeStamp, block.Data, block.PreHash}, []byte{}))
+// 	// block.GiveHash(h)
+// 	block.DeriveHash()
+// 	return block
+// }
+
+// func (chain *BlockChain) AddBlock(data string) {
+// 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+// 	new := CreatBlock(data, prevBlock.Hash)
+// 	new.Mine()
+// 	chain.Blocks = append(chain.Blocks, new)
+// }
+
+// func Genesis() *Block {
+// 	return CreatBlock("Genesis", []byte{})
+// }
+
+// func InitBlockChain() *BlockChain {
+// 	return &BlockChain{[]*Block{Genesis()}}
+// }
+
+// func ComparePassword(hashedPassword []byte, password []byte) error {
+// 	er := bcrypt.CompareHashAndPassword(hashedPassword, password)
+// 	if er != nil {
+// 		return er
+// 	}
+// 	return nil
+// }
+
+// func Hash1(data []byte) []byte {
+// 	hash := sha256.Sum256(data)
+// 	return hash[:]
+// }
+
+// func (b *Block) Mine() {
+// 	// h := Hash1(b.Hash)
+// 	for string(b.Hash[0:1]) != "0" {
+// 		// fmt.Println([]byte("0"))
+// 		// fmt.Println(string(h[0:1]))
+// 		b.Nonce++
+// 		// h = Hash1(h)
+// 		b.DeriveHash()
+// 	}
+// 	// b.GiveHash(h)
+// }
+
+// func (chain *BlockChain) IsValid() bool {
+// 	for i := 1; i < len(chain.Blocks); i++ {
+// 		currentBlock := chain.Blocks[i]
+// 		preBlock := chain.Blocks[i-1]
+// 		if string(currentBlock.Hash) != string(currentBlock.CalculateHash()) {
+// 			return false
+// 		}
+
+// 		if string(currentBlock.PreHash) != string(preBlock.Hash) {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
 
 type BlockChain struct {
 	Blocks []*Block
 }
 
-func (b *Block) DeriveHash() {
-	times := time.Now().String()
-	info := bytes.Join([][]byte{[]byte(times), b.Data, b.PreHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-	b.TimeStamp = []byte(times)
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+	Nonce    int
 }
 
-// func (b *Block) GiveHash(h []byte) {
-// 	b.Hash = h[:]
-// }
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
 
-func (b *Block) CalculateHash() []byte {
-	info := bytes.Join([][]byte{b.TimeStamp, b.Data, b.PreHash}, []byte{})
-	hash := sha256.Sum256(info)
-	h := hash[:]
-	return h
-}
+	block.Hash = hash[:]
+	block.Nonce = nonce
 
-func CreatBlock(data string, preHash []byte) *Block {
-
-	block := &Block{[]byte{}, []byte{}, []byte(data), preHash, 0}
-	// h := Hash1(bytes.Join([][]byte{block.TimeStamp, block.Data, block.PreHash}, []byte{}))
-	// block.GiveHash(h)
-	block.DeriveHash()
 	return block
 }
 
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreatBlock(data, prevBlock.Hash)
-	new.Mine()
+	new := CreateBlock(data, prevBlock.Hash)
 	chain.Blocks = append(chain.Blocks, new)
 }
 
 func Genesis() *Block {
-	return CreatBlock("Genesis", []byte{})
+	return CreateBlock("Genesis", []byte{})
 }
 
 func InitBlockChain() *BlockChain {
 	return &BlockChain{[]*Block{Genesis()}}
-}
-
-func ComparePassword(hashedPassword []byte, password []byte) error {
-	er := bcrypt.CompareHashAndPassword(hashedPassword, password)
-	if er != nil {
-		return er
-	}
-	return nil
-}
-
-func Hash1(data []byte) []byte {
-	hash := sha256.Sum256(data)
-	return hash[:]
-}
-
-func (b *Block) Mine() {
-	// h := Hash1(b.Hash)
-	for string(b.Hash[0:1]) != "0" {
-		// fmt.Println([]byte("0"))
-		// fmt.Println(string(h[0:1]))
-		b.Nonce++
-		// h = Hash1(h)
-		b.DeriveHash()
-	}
-	// b.GiveHash(h)
-}
-
-func (chain *BlockChain) IsValid() bool {
-	for i := 1; i < len(chain.Blocks); i++ {
-		currentBlock := chain.Blocks[i]
-		preBlock := chain.Blocks[i-1]
-		if string(currentBlock.Hash) != string(currentBlock.CalculateHash()) {
-			return false
-		}
-
-		if string(currentBlock.PreHash) != string(preBlock.Hash) {
-			return false
-		}
-	}
-	return true
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,55 @@
 package main
 
+// import (
+// 	"fmt"
+
+// 	"github.com/namle133/go-blockchain.git/go-blockchain/blockchain"
+// 	// "strconv"
+// )
+
+// func main() {
+// 	// fmt.Println(string([]byte{0}))
+// 	// data := []byte("hello")
+// 	// hash := sha256.Sum256(data)
+// 	// fmt.Printf("%x", hash[:])
+
+// 	chain := blockchain.InitBlockChain()
+
+// 	chain.AddBlock("First Block after Genesis")
+// 	chain.AddBlock("Second Block after Genesis")
+// 	chain.AddBlock("Third Block after Genesis")
+
+// 	for _, block := range chain.Blocks {
+// 		fmt.Printf("Previous Hash: %x\n", block.PreHash)
+// 		fmt.Printf("Data in Block: %s\n", block.Data)
+// 		fmt.Printf("Hash: %x\n", block.Hash)
+// 		// fmt.Println(strconv.Itoa(block.Hash))
+// 		// fmt.Println(reflect.TypeOf(block.Hash))
+// 		// fmt.Println(block.Hash)
+// 		fmt.Printf("Nonce: %v\n", block.Nonce)
+// 		fmt.Printf("Time: %s\n", block.TimeStamp)
+
+// 	}
+// 	fmt.Printf("valid: %v\n\n", chain.IsValid())
+// 	chain.Blocks[1].Data = []byte("Namle")
+// 	chain.Blocks[1].Hash = chain.Blocks[1].CalculateHash()
+// 	// for _, block := range chain.Blocks {
+// 	// 	fmt.Printf("Previous Hash: %x\n", block.PreHash)
+// 	// 	fmt.Printf("Data in Block: %s\n", block.Data)
+// 	// 	fmt.Printf("Hash: %x\n", block.Hash)
+// 	// }
+// 	fmt.Printf("valid: %v\n\n", chain.IsValid())
+
+// }
+
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/namle133/go-blockchain.git/go-blockchain/blockchain"
-	// "strconv"
 )
 
 func main() {
-	// fmt.Println(string([]byte{0}))
-	// data := []byte("hello")
-	// hash := sha256.Sum256(data)
-	// fmt.Printf("%x", hash[:])
-
 	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
@@ -20,24 +57,15 @@ func main() {
 	chain.AddBlock("Third Block after Genesis")
 
 	for _, block := range chain.Blocks {
-		fmt.Printf("Previous Hash: %x\n", block.PreHash)
+
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
-		// fmt.Println(strconv.Itoa(block.Hash))
-		// fmt.Println(reflect.TypeOf(block.Hash))
-		// fmt.Println(block.Hash)
-		fmt.Printf("Nonce: %v\n", block.Nonce)
-		fmt.Printf("Time: %s\n", block.TimeStamp)
+		fmt.Printf("Nonce: %d\n", block.Nonce)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 
 	}
-	fmt.Printf("valid: %v\n\n", chain.IsValid())
-	chain.Blocks[1].Data = []byte("Namle")
-	chain.Blocks[1].Hash = chain.Blocks[1].CalculateHash()
-	// for _, block := range chain.Blocks {
-	// 	fmt.Printf("Previous Hash: %x\n", block.PreHash)
-	// 	fmt.Printf("Data in Block: %s\n", block.Data)
-	// 	fmt.Printf("Hash: %x\n", block.Hash)
-	// }
-	fmt.Printf("valid: %v\n\n", chain.IsValid())
-
 }
